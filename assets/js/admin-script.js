90ba2c7906d6bb0ce0844c5be521c3b75b391c18
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-05-07 09:40:22 -0400

    fix: add class onload if title is disabled

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -1,5 +1,20 @@
 jQuery(function($) {
+
+    const interval = setInterval( function() {
+        if ( $('.editor-styles-wrapper').length !== 0 ) {
+            setupWrapperClass();
+        }
+    }, 50);
+
+    function setupWrapperClass () {
+        clearInterval(interval);
+        if ( acf.getField('field_smvmt2020_appearance_disable_title').val() === 1 ) {
+            $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--title-disabled');
+        }
+    }
+
     $('[name="acf[field_smvmt2020_appearance_disable_title]"]').change(function () {
         $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--title-disabled');
     });
+    
 });
