9d2a37af851674645c57ff30afff222500dd63f1
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-05-07 09:58:02 -0400

    feature: support disable top spacing in editor

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -11,10 +11,17 @@ jQuery(function($) {
         if ( acf.getField('field_smvmt2020_appearance_disable_title').val() === 1 ) {
             $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--title-disabled');
         }
+        if ( acf.getField('field_smvmt2020_appearance_disable_top_spacing').val() === 1 ) {
+            $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--top-spacing-disabled');
+        }
     }
 
     $('[name="acf[field_smvmt2020_appearance_disable_title]"]').change(function () {
         $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--title-disabled');
     });
-    
+
+    $('[name="acf[field_smvmt2020_appearance_disable_top_spacing]"]').change(function () {
+        $('.editor-styles-wrapper').toggleClass('editor-styles-wrapper--top-spacing-disabled');
+    });
+
 });
