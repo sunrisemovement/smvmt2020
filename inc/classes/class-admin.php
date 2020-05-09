80e1430c8d78f551a0cc9c5b175611945f71db15
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-05-09 14:09:33 -0400

    feature: restyle editor typography

--- inc/classes/class-admin.php
+++ inc/classes/class-admin.php
@@ -15,5 +15,15 @@ class Admin {
     public function loadEditorAssets () {
         wp_enqueue_script( 'smvmt2020-admin-script', get_stylesheet_directory_uri().'/assets/js/admin-script.js', ['jquery'] );
         wp_enqueue_style( 'smvmt2020-admin-style', get_stylesheet_directory_uri().'/assets/css/admin-style.css' );
+        wp_enqueue_style(
+            'smvmt2020-source-serif-pro',
+            'https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@600;700&display=swap',
+            false
+        );
+        wp_enqueue_style(
+            'smvmt2020-source-sans-pro',
+            'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700;900&display=swap',
+            false
+        );
     }
 }
